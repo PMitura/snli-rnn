7094579e20c0970797e0787ef69b7cab8bb8be51
Peter Mitura <pmitura.jr@gmail.com>
2017-12-12 00:59:40 +0100

    Batch generation and embedding lookup.

--- rnn/trainer.py
+++ rnn/trainer.py
@@ -19,25 +19,52 @@ def load_embedding_matrix():
     with open(prep.PRECOMPUTED_EMB_MATRIX_PATH, 'r') as matrix_file:
         matrix = json.load(matrix_file)
     np_matrix = np.array(matrix, np.float32)
-    print(np_matrix.shape)
     tf_matrix = tf.get_variable(name="tf_matrix", shape=np_matrix.shape,
                                 initializer=tf.constant_initializer(np_matrix), trainable=False)
     return tf_matrix
 
 
+def load_padded_matrix(path):
+    with open(path, 'r') as file:
+        rows = json.load(file)
+    maxlen = 0
+    for row in rows:
+        maxlen = max(maxlen, len(row))
+    matrix = np.zeros((len(rows), maxlen))
+    for idx, row in enumerate(rows):
+        matrix[idx] = np.pad(row, (0, maxlen - len(row)), mode='constant')
+    return matrix
+
+
 def load_train_matrices():
-    if not os.path.exists(prep.PRECOMPUTED_TRAIN_HYPOTHESES_PATH)\
-            or not os.path.exists(prep.PRECOMPUTED_TRAIN_PREMISES_PATH):
+    if not os.path.exists(prep.PRECOMPUTED_TRAIN_HYPOTHESES_PATH) \
+            or not os.path.exists(prep.PRECOMPUTED_TRAIN_PREMISES_PATH) \
+            or not os.path.exists(prep.PRECOMPUTED_TRAIN_LABELS_PATH):
         logger.error("Training matrices not found, please run preprocessor first.")
         sys.exit(1)
-    with open(prep.PRECOMPUTED_TRAIN_PREMISES_PATH, 'r') as premises_file:
-        premise = json.load(premises_file)
-    with open(prep.PRECOMPUTED_TRAIN_HYPOTHESES_PATH, 'r') as hypotheses_file:
-        hypothesis = json.load(hypotheses_file)
-    return premise, hypothesis
+    premise = load_padded_matrix(prep.PRECOMPUTED_TRAIN_PREMISES_PATH)
+    hypothesis = load_padded_matrix(prep.PRECOMPUTED_TRAIN_HYPOTHESES_PATH)
+    with open(prep.PRECOMPUTED_TRAIN_LABELS_PATH, 'r') as labels_file:
+        labels = json.load(labels_file)
+    return premise, hypothesis, labels
 
 
-# def create_batch_producer(matrix, train_)
+def produce_batch(premise, hypothesis, labels, batch_size=32, name=None):
+    with tf.name_scope(name, "producers", [premise, hypothesis, labels, batch_size]):
+        premise = tf.convert_to_tensor(premise, name="premise", dtype=tf.int32)
+        hypothesis = tf.convert_to_tensor(hypothesis, name="hypothesis", dtype=tf.int32)
+        labels = tf.convert_to_tensor(labels, name="labels", dtype=tf.int32)
+
+        input_len = tf.shape(premise)[0]
+        num_steps = tf.shape(premise)[1]
+        batch_len = input_len // batch_size
+
+        i = tf.train.range_input_producer(limit=batch_len, shuffle=False).dequeue()
+        premise_batch = tf.slice(premise, [i * batch_size, 0], [batch_size, num_steps], name="premise_batch")
+        hypothesis_batch = tf.slice(hypothesis, [i * batch_size, 0], [batch_size, num_steps], name="hypotheis_batch")
+        labels_batch = tf.slice(labels, [i * batch_size], [batch_size], name="labels_batch")
+
+        return premise_batch, hypothesis_batch, labels_batch
 
 
 def run():
@@ -45,13 +72,14 @@ def run():
 
     logger.info("Loading embedding matrix into tensorflow model")
     embedding_matrix = load_embedding_matrix()
-    logger.info("Matrix loaded")
+    logger.success("Matrix loaded")
 
-    logger.info("Loading training data matrix")
-    train_premise_matrix, train_hypothesis_matrix = load_train_matrices()
-    logger.info("Matrix loaded")
+    logger.info("Loading training data matrices")
+    premise_matrix, hypothesis_matrix, labels = load_train_matrices()
+    logger.success("Matrices loaded")
 
     logger.info("Building tensorflow model")
-    # word_embeddings = tf.nn.embedding_lookup(embedding_matrix, )
-    logger.info("Model built")
+    premise_batch, hypothesis_batch, label_batch = produce_batch(premise_matrix, hypothesis_matrix, labels)
+    word_embeddings = tf.nn.embedding_lookup(embedding_matrix, premise_batch)
+    logger.success("Model built")
 
